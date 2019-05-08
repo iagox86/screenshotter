c8667de2f3aec0b9bfc1d399c2c176e0f903337e
iagox86 <ron-git@skullsecurity.org>
2019-05-07 18:40:32 -0700

    Handle an arbitrary number of files

--- screenshotter.rb
+++ screenshotter.rb
@@ -1,42 +1,44 @@
 #!/usr/bin/env ruby
 
-FILE1 = ARGV[0]
-FILE2 = ARGV[1]
-N = (ARGV[2] || 10).to_i
+N = (ARGV[0]).to_i
 
-if(!FILE2)
-  puts("Usage: screenshotter.rb <FILE1> <FILE2> [number of snaps]")
+FILES = ARGV[1..-1]
+
+if(FILES.length == 0)
+  puts("Usage: screenshotter.rb <Number of snaps> <FILE1> [<FILE2> ...]")
   exit
 end
 
-REAL_LENGTH1 = `ffmpeg -i "#{FILE1}" 2>&1 | grep "Duration"`.scan(/Duration: ([0-9]+:[0-9]+:[0-9]+)/).pop.pop
-REAL_LENGTH2 = `ffmpeg -i "#{FILE2}" 2>&1 | grep "Duration"`.scan(/Duration: ([0-9]+:[0-9]+:[0-9]+)/).pop.pop
+REAL_LENGTHS = FILES.map do |f|
+  `ffmpeg -i "#{f}" 2>&1 | grep "Duration"`.scan(/Duration: ([0-9]+:[0-9]+:[0-9]+)/).pop.pop
+end
 
-LENGTH1 = REAL_LENGTH1.split(':').map { |a| a.to_i }.inject(0) { |a, b| a * 60 + b }
-LENGTH2 = REAL_LENGTH2.split(':').map { |a| a.to_i }.inject(0) { |a, b| a * 60 + b }
+LENGTHS = REAL_LENGTHS.map do |l|
+  l.split(':').map { |a| a.to_i }.inject(0) { |a, b| a * 60 + b }
+end
 
-
-puts "Comparing #{N} shots from:"
-puts "* #{FILE1} -- #{REAL_LENGTH1} -- #{LENGTH1}s"
-puts "* #{FILE2} -- #{REAL_LENGTH2} -- #{LENGTH2}s"
+puts "Taking #{N} shots from:"
+0.upto(FILES.length - 1) do |i|
+  puts "* #{FILES[i]} -- #{REAL_LENGTHS[i]} -- #{LENGTHS[i]}s"
+end
 puts
 
-if(REAL_LENGTH1 != REAL_LENGTH2)
-  puts
-  puts("** Warning: videos are not the same length, probably due to different frame rates. YMMV!")
-  puts
-  puts "(press enter to continue)"
-  gets
+1.upto(FILES.length - 1) do |i|
+  if(REAL_LENGTHS[i - 1] != REAL_LENGTHS[i])
+    puts("** Warning: videos are not the same length, probably due to different frame rates. YMMV!")
+    puts
+    puts "(press enter to continue)"
+    $stdin.gets
+  end
 end
 
 0.upto(N-1) do
-  timestamp = Time.at(rand([LENGTH1, LENGTH2].min)).utc.strftime("%H:%M:%S")
+  timestamp = Time.at(rand(LENGTHS.min)).utc.strftime("%H:%M:%S")
 
-  name = "#{timestamp}-a.png".gsub(/:/, '-')
-  puts "Snapping #{FILE1} @ #{timestamp} -> #{name}"
-  `ffmpeg -ss #{timestamp} -i "#{FILE1}" -vframes 1 "#{name}" 2>&1 > /dev/null`
-
-  name = "#{timestamp}-b.png".gsub(/:/, '-')
-  puts "Snapping #{FILE2} @ #{timestamp} -> #{name}"
-  `ffmpeg -ss #{timestamp} -i "#{FILE2}" -vframes 1 "#{name}" 2>&1 > /dev/null`
+  0.upto(FILES.length - 1) do |i|
+    name = "#{timestamp}-#{(0x61 + i).chr}.png".gsub(/:/, '-')
+    puts "Snapping #{FILES[i]} @ #{timestamp} -> #{name}"
+    `ffmpeg -ss #{timestamp} -i "#{FILES[i]}" -vframes 1 "#{name}" 2>&1 > /dev/null`
+  end
+  puts
 end
