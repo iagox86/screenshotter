559145582971b7f316156a271b46199cbfc9412a
iagox86 <ron-git@skullsecurity.org>
2019-05-08 14:30:31 -0700

    Use frames and FPS instead of seconds, so we get a more accurate timestamp

--- screenshotter.rb
+++ screenshotter.rb
@@ -2,40 +2,29 @@
 
 N = (ARGV[0]).to_i
 
-FILES = ARGV[1..-1].map { |f| "file:#{f}" }
+FILES = ARGV[1..-1]
 
 if(FILES.length == 0)
   puts("Usage: screenshotter.rb <Number of snaps> <FILE1> [<FILE2> ...]")
   exit
 end
 
-REAL_LENGTHS = FILES.map do |f|
-  `ffmpeg -i "#{f}" 2>&1 | grep "Duration"`.scan(/Duration: ([0-9]+:[0-9]+:[0-9]+\.[0-9]+)/).pop.pop
+INFO = FILES.map do |f|
+  {
+    filename:   f,
+    framecount: `mediainfo --Output='Video;%FrameCount%' '#{f}'`.chomp.to_i,
+    framerate: `mediainfo --Output='Video;%FrameRate%' '#{f}'`.chomp.to_f,
+    ratio:      `ffprobe -v error -show_format -show_streams 'file:#{f}'  | grep display_aspect_ratio`.split(/=/)[1].chomp.split(/:/).map { |i| i.to_i },
+    width:      `ffprobe -v error -show_format -show_streams 'file:#{f}'  | grep '^width='`.split(/=/)[1].chomp.to_i,
+  }
 end
-
-LENGTHS = REAL_LENGTHS.map do |l|
-  h, m, s, cs = l.scan(/([0-9]+):([0-9]+):([0-9]+)\.([0-9]+)/).pop
-
-  cs.to_i*10 + s.to_i*1000 + m.to_i*60000 + h.to_i*3600000
-end
-
-RATIOS = FILES.map do |f|
-  `ffprobe -v error -show_format -show_streams '#{f}'  | grep display_aspect_ratio`.split(/=/)[1].chomp.split(/:/).map { |i| i.to_i }
-end
-
-WIDTHS = FILES.map do |f|
-  `ffprobe -v error -show_format -show_streams '#{f}'  | grep '^width='`.split(/=/)[1].chomp.to_i
-end
-
 
 puts "Taking #{N} shots from:"
-0.upto(FILES.length - 1) do |i|
-  puts "* #{REAL_LENGTHS[i]} -- #{LENGTHS[i]}ms -- #{FILES[i]} "
-end
+puts INFO
 puts
 
 1.upto(FILES.length - 1) do |i|
-  if(LENGTHS[i - 1] != LENGTHS[i])
+  if(INFO[i - 1][:framecount] != INFO[i][:framecount])
     puts("** Warning: videos are not the same length, probably due to different frame rates. YMMV!")
     puts
     puts "(press enter to continue)"
@@ -43,32 +32,20 @@ puts
   end
 end
 
-0.upto(N-1) do
-  base_timestamp = rand(LENGTHS.min)
+0.upto(N-1) do |c|
+  location = rand()
 
   0.upto(FILES.length - 1) do |i|
-    ms = base_timestamp
+    frame = (location * INFO[i][:framecount]).floor
 
-    h  = ms / 3600000
-    ms = ms % 3600000
+    name = "shot#{c}-#{(0x61 + i).chr}.png".gsub(/:/, '-')
+    puts "Snapping #{INFO[i][:filename]} @ #{frame} -> #{name}"
 
-    m  = ms / 60000
-    ms = ms % 60000
+    width = INFO[i][:width]
+    height = width * INFO[i][:ratio][1] / INFO[i][:ratio][0]
 
-    s  = ms / 1000
-    ms = ms % 1000
-
-    formatted = "%02d:%02d:%02d.%02d" % [h, m, s, ms / 10]
-
-
-    name = "#{formatted}-#{(0x61 + i).chr}.png".gsub(/:/, '-')
-    puts "Snapping #{FILES[i]} @ #{formatted} -> #{name}"
-
-    width = WIDTHS[i]
-    height = width * RATIOS[i][1] / RATIOS[i][0]
-
-    command = "ffmpeg -ss #{formatted} -i '#{FILES[i]}' -vframes 1 -vf scale=#{width}x#{height} '#{name}' 2>&1 > /dev/null"
-    puts command
+    command = "yes|ffmpeg -ss '#{location * INFO[i][:framecount] / INFO[i][:framerate]}' -i 'file:#{INFO[i][:filename]}' -vframes 1 -vf 'scale=#{width}x#{height}'  '#{name}' 2>&1 > /dev/null"
+    puts "#{command}..."
     `#{command}`
   end
   puts
